d2b4a52e00348d4dd873de368cda279bc72be3d9
Elkin Alexander Giraldo Ordoñez <alex_giraldo@hotmail.com>
2021-01-07 10:23:21 -0500

    Version 1.0

--- ConsoleAppPeau-k/Program.cs
+++ ConsoleAppPeau-k/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleAppPeau_k
     {
         static void Main(string[] args)
         {
+            string prueba = "Able was I ere I saw Elba";
+            Acciones obj = new Acciones();
+            bool resultado = obj.Palindrome(prueba);
+        }
+
+
+    }
+
+    public class Acciones
+    {
+        public bool Palindrome(string cadena)
+        {
+            char[] data = cadena.ToArray();
+            int n = data.Length-1;
+            int lim = n / 2;
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (data[i].ToString().ToUpper() != data[n].ToString().ToUpper())
+                {
+                    return false;
+                }
+
+                n--;
+            }
+
+            return true;
         }
     }
 }
